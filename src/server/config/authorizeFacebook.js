584e47059b67e5a8e4a26143208a6ddf0c9f096d
Cathy <caathylee@gmail.com>
2016-06-09 14:03:22 -0700

    Button click now goes to Facebook page for login

---
+++ src/server/config/authorizeFacebook.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '283654828635162', // your App ID
+        'clientSecret'  : 'a88c3ab15af3494fe631698fba934f12', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+    }
+
+    // 'twitterAuth' : {
+    //     'consumerKey'       : 'your-consumer-key-here',
+    //     'consumerSecret'    : 'your-client-secret-here',
+    //     'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    // },
+
+    // 'googleAuth' : {
+    //     'clientID'      : 'your-secret-clientID-here',
+    //     'clientSecret'  : 'your-client-secret-here',
+    //     'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    // }
+
+};
